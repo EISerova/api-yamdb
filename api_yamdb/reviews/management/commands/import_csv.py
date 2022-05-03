d9bb0306c39fcfe5e6ea2a932eb9974c4627ca55
Екатерина Серова <katyaserova@yandex.ru>
2022-05-03 23:54:54 +0300

    ready for review

--- api_yamdb/reviews/management/commands/import_csv.py
+++ api_yamdb/reviews/management/commands/import_csv.py
@@ -8,7 +8,7 @@ class Command(BaseCommand):
     help = 'Запись в БД данных из csv-файлов'
 
     FILES = {
-        Title: 'titles.csv',
+        Title: 'titles1.csv',
         Comment: 'comments.csv',
         User: 'users.csv',
         Category: 'category.csv',
@@ -16,86 +16,84 @@ class Command(BaseCommand):
         Review: 'review.csv',
     }
 
-    def handle(self, *args, **kwargs):  # noqa: C901,
+    def review_create(row):
+        try:
+            Review.objects.get_or_create(
+                id=row['id'],
+                text=row['text'],
+                title_id=row['title_id'],
+                author_id=row['author'],
+                score=row['score'],
+                pub_date=row['pub_date'],
+            )
+        except Exception as error:
+            message = f'Ошибка - {error}, проблема в строке - {row}'
+            raise CommandError(message)
+
+    def comment_create(row):
+        try:
+            Comment.objects.get_or_create(
+                id=row['id'],
+                text=row['text'],
+                review_id=row['review_id'],
+                author_id=row['author'],
+                pub_date=row['pub_date'],
+            )
+        except Exception as error:
+            message = f'Ошибка - {error}, проблема в строке - {row}'
+        raise CommandError(message)
+
+    def title_create(row):
+        try:
+            Title.objects.get_or_create(
+                id=row['id'],
+                name=row['name'],
+                year=row['year'],
+                category_id=row['category'],
+            )
+        except Exception as error:
+            message = f'Ошибка - {error}, проблема в строке - {row}'
+            raise CommandError(message)
+
+    def review_create(row):
+        try:
+            Review.objects.get_or_create(
+                id=row['id'],
+                text=row['text'],
+                title_id=row['title_id'],
+                author_id=row['author'],
+                score=row['score'],
+                pub_date=row['pub_date'],
+            )
+        except Exception as error:
+            message = f'Ошибка - {error}, проблема в строке - {row}'
+            raise CommandError(message)
+
+    MODEL_CREATE_FUNC = {
+        'Review': review_create,
+        'Comment': comment_create,
+        'Title': title_create,
+        'Review': review_create,
+    }
+
+    def handle(self, *args, **kwargs):
         for model, file in self.FILES.items():
             with open(
                 f'static/data/{file}', 'rt', encoding='utf-8'
             ) as csv_file:
                 csv_reader = csv.DictReader(csv_file)
 
-                if model._meta.object_name == 'Review':
-                    for row in csv_reader:
-                        try:
-                            Review.objects.get_or_create(
-                                id=row['id'],
-                                text=row['text'],
-                                title_id=row['title_id'],
-                                author_id=row['author'],
-                                score=row['score'],
-                                pub_date=row['pub_date'],
-                            )
-                        except Exception as error:
-                            message = (
-                                f'Ошибка - {error}, проблема в строке - {row}'
-                            )
-                            raise CommandError(message)
-
-                if model._meta.object_name == 'Comment':
-                    for row in csv_reader:
-                        try:
-                            Comment.objects.get_or_create(
-                                id=row['id'],
-                                text=row['text'],
-                                review_id=row['review_id'],
-                                author_id=row['author'],
-                                pub_date=row['pub_date'],
-                            )
-                        except Exception as error:
-                            message = (
-                                f'Ошибка - {error}, проблема в строке - {row}'
-                            )
-                            raise CommandError(message)
-
-                if model._meta.object_name == 'Title':
-                    for row in csv_reader:
-                        try:
-                            model.objects.get_or_create(
-                                id=row['id'],
-                                name=row['name'],
-                                year=row['year'],
-                                category_id=row['category'],
-                            )
-                        except Exception as error:
-                            message = (
-                                f'Ошибка - {error}, проблема в строке - {row}'
-                            )
-                            raise CommandError(message)
-
-                if model._meta.object_name == 'Review':
-                    for row in csv_reader:
-                        try:
-                            Review.objects.get_or_create(
-                                id=row['id'],
-                                text=row['text'],
-                                title_id=row['title_id'],
-                                author_id=row['author'],
-                                score=row['score'],
-                                pub_date=row['pub_date'],
-                            )
-                        except Exception as error:
-                            message = (
-                                f'Ошибка - {error}, проблема в строке - {row}'
-                            )
-                            raise CommandError(message)
-
-                else:
-                    try:
-                        model.objects.bulk_create(
-                            model(**row) for row in csv_reader
-                        )
-                    except Exception as error:
-                        message = f'Ошибка - {error}'
-                        raise CommandError(message)
+                for name, func in self.MODEL_CREATE_FUNC.items():
+                    if model._meta.object_name == name:
+                        for row in csv_reader:
+                            func(row)
+                try:
+                    model.objects.bulk_create(
+                        model(**row) for row in csv_reader
+                    )
+                except Exception as error:
+                    message = f'Ошибка - {error}'
+                    raise CommandError(message)
 
                 self.stdout.write(
                     f'Данные из файлов перенесены в базу {model}.'
